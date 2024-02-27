c2bb497bf1636270afc9ca70503a9c38313bbf72
Mord100 <chingolo265@gmail.com>
2024-02-27 15:33:06 +0200

    tuesday commit

--- frontend/src/components/ListItems.jsx
+++ frontend/src/components/ListItems.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineEdit, AiOutlineDelete, AiOutlineCheckCircle } from 'react-icons/ai';
 
-const ListItems = ({task}) => {
-  return (
-    <p>
-        {task.title}
-    </p>
-  )
+const ListItems = ({ task }) => {
+    return (
+        <div className="list-item">
+             <div className="info-container">
+                <AiOutlineCheckCircle className="tick-icon" /> {/* Tick Icon */}
+                <p> {task.title}</p>  
+            </div>
+
+            <div className="icon-container">
+                <button className='edit'><AiOutlineEdit /> Edit</button> {/* Edit Icon */}
+                <button className='delete'><AiOutlineDelete /> Delete</button> {/* Delete Icon */}
+            </div>
+        </div>
+    )
 }
 
-export default ListItems
+export default ListItems;
